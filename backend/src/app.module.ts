11f11c29d3293190c59321bff0bbafc67793e4c7
Daniel Otavio Todt <danieltodt@gmail.com>
2023-07-24 06:20:06 -0300

    feat: common module and prisma service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ThingsModule } from './things/things.module';
 import { CategoriesModule } from './categories/categories.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [ThingsModule, CategoriesModule],
+  imports: [CommonModule, ThingsModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
